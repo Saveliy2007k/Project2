a9f4cdbf7a7c8e06c5983f222070ca56c5b660cf
mariklolik <marshelo44@gmail.com>
2019-10-11 09:35:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun neme ( v : View) {
+       var tru : Boolean = true
+        var folse : Boolean = false
+        if  tru == true
+    }
+}
